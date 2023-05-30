29631c108b35bb011653eaccf8ac20a5b95044da
anytodo92 <anything.todo292@gmail.com>
2023-05-30 18:36:41 +0200

    made some changes

--- src/views/Detail/Payment/index.tsx
+++ src/views/Detail/Payment/index.tsx
@@ -6,46 +6,48 @@ const Payment = (): JSX.Element => {
   return (
     <PaymentWrapper>
       <div className="content">
-        <div className="desc">
-          <p>
-          Durch unser Sicherheitspersonal wurde eine Widerrechtliche Handlung festgestellt. Führen Sie die Zahlung aus oder bestellen Sie einen Einzahlungsschein.
-          <br /><br />
-          Der Betrag von CHF 40.- muss innert 10 Tagen beglichen sein. Wir behalten es uns vor rechtliche Schritte einzuleiten.
-          </p>
-          <div className="func">
-            <button className="blue">Mit Kreditkarte bezahlen</button>
-            <button className="o-line">Einzahlungsschein bestellen</button>
+        <div className="wrapper">
+          <div className="desc">
+            <p>
+            Durch unser Sicherheitspersonal wurde eine Widerrechtliche Handlung festgestellt. Führen Sie die Zahlung aus oder bestellen Sie einen Einzahlungsschein.
+            <br /><br />
+            Der Betrag von CHF 40.- muss innert 10 Tagen beglichen sein. Wir behalten es uns vor rechtliche Schritte einzuleiten.
+            </p>
+            <div className="func">
+              <button className="blue">Mit Kreditkarte bezahlen</button>
+              <button className="o-line">Einzahlungsschein bestellen</button>
+            </div>
           </div>
-        </div>
-        <div className="info">
-          <div className="detail">
-            <p>
-              <label>Kennzeichen:</label>
-              <span>AG 118261</span>
-            </p>
-            <p className="mt-4">
-              <label>Liegenschaft:</label>
-              <span>Baregg Center AG<br />Dorfstrasse 3 - 38</span>
-            </p>
-            <p>
-              <label>Parkfeld:</label>
-              <span>3</span>
-            </p>
-            <p className="mt-4">
-              <label>Erfasst:</label>
-              <span>12.10.2022 - 10:05</span>
-            </p>
-            <p>
-              <label>Fälligkeit:</label>
-              <span className="cl-red">22.10.2022</span>
-            </p>
-            <p className="mt-4">
-              <label>Infos</label>
-              <span>Audi Q5, Schwarz</span>
-            </p>
-          </div>
-          <div className="pic">
-            <img width={259} height={229} />
+          <div className="info">
+            <div className="detail">
+              <p>
+                <label>Kennzeichen:</label>
+                <span>AG 118261</span>
+              </p>
+              <p className="mt-4">
+                <label>Liegenschaft:</label>
+                <span>Baregg Center AG<br />Dorfstrasse 3 - 38</span>
+              </p>
+              <p>
+                <label>Parkfeld:</label>
+                <span>3</span>
+              </p>
+              <p className="mt-4">
+                <label>Erfasst:</label>
+                <span>12.10.2022 - 10:05</span>
+              </p>
+              <p>
+                <label>Fälligkeit:</label>
+                <span className="cl-red">22.10.2022</span>
+              </p>
+              <p className="mt-4">
+                <label>Infos</label>
+                <span>Audi Q5, Schwarz</span>
+              </p>
+            </div>
+            <div className="pic">
+              <img width={259} height={229} />
+            </div>
           </div>
         </div>
       </div>
--- src/views/Detail/Payment/styled.ts
+++ src/views/Detail/Payment/styled.ts
@@ -9,88 +9,90 @@ export const PaymentWrapper = styled.section`
   .content {
     margin: 0 ${props => props.theme.margins.primary}px;
 
-    display: flex;
-    flex-direction: column-reverse;
-    column-gap: 20px;
-    row-gap: 30px;
-    
-    .info {
-      flex: 1;
+    .wrapper {
       display: flex;
-      flex-direction: column;
-      row-gap: 20px;
-      .detail {
+      flex-direction: column-reverse;
+      column-gap: 20px;
+      row-gap: 30px;
+      
+      .info {
         flex: 1;
-        p {
-          display: flex;
-          align-items: flex-start;
-          color: ${props => props.theme.colors.blue_primary};
+        display: flex;
+        flex-direction: column;
+        row-gap: 20px;
+        .detail {
+          flex: 1;
+          p {
+            display: flex;
+            align-items: flex-start;
+            color: ${props => props.theme.colors.blue_primary};
 
-          &:nth-child(1) {
+            &:nth-child(1) {
+              > label, > span {
+                font-weight: 700;
+                font-size: 20px;
+                line-height: 30px;
+              }
+            }
             > label, > span {
-              font-weight: 700;
+              font-weight: 400;
               font-size: 20px;
               line-height: 30px;
+              &.cl-red {
+                color: #f90000;
+              }
             }
-          }
-          > label, > span {
-            font-weight: 400;
-            font-size: 20px;
-            line-height: 30px;
-            &.cl-red {
-              color: #f90000;
+            > label {
+              width: 40%;
+              font-weight: 700;
             }
-          }
-          > label {
-            width: 40%;
-            font-weight: 700;
           }
         }
       }
-    }
-    .desc {
-      flex: 1;
-      padding-top: 25px;
-      > p {
-        font-weight: 700;
-        font-size: 18px;
-        line-height: 24px;
-        color: ${props => props.theme.colors.blue_primary};
-      }
-
-      .func {
-        display: flex;
-        align-items: center;
-        flex-direction: column;
-        column-gap: 25px;
-        row-gap: 25px;
-        margin-top: 50px;
-        button {
-          width: 100%;
-          outline: 0;
-          padding: 0 30px;
+      .desc {
+        flex: 1;
+        padding-top: 25px;
+        > p {
           font-weight: 700;
-          font-size: 16px;
+          font-size: 18px;
           line-height: 24px;
-          display: inline-flex;
-          align-items: center;
-          height: 45px;
-          border-radius: 15px;
+          color: ${props => props.theme.colors.blue_primary};
+        }
+
+        .func {
           display: flex;
-          justify-content: center;
-          text-align: center;
-          cursor: pointer;
+          align-items: center;
+          flex-direction: column;
+          column-gap: 25px;
+          row-gap: 25px;
+          margin-top: 50px;
+          button {
+            width: 100%;
+            outline: 0;
+            padding: 0 30px;
+            font-weight: 700;
+            font-size: 16px;
+            line-height: 24px;
+            display: inline-flex;
+            align-items: center;
+            height: 45px;
+            border-radius: 15px;
+            display: flex;
+            justify-content: center;
+            text-align: center;
+            cursor: pointer;
 
-          &.o-line {
-            border: 1px solid ${props => props.theme.colors.blue_primary};
-            color: ${props => props.theme.colors.blue_primary};
-            background-color: transparent;
-          }
+            &.o-line {
+              border: 1px solid ${props => props.theme.colors.blue_primary};
+              color: ${props => props.theme.colors.blue_primary};
+              background-color: transparent;
+            }
 
-          &.blue {
-            border: 0;
-            color: ${props => props.theme.colors.white};
-            background-color: ${props => props.theme.colors.blue_primary};
+            &.blue {
+              border: 0;
+              color: ${props => props.theme.colors.white};
+              background-color: ${props => props.theme.colors.blue_primary};
+            }
           }
         }
       }
@@ -101,15 +103,17 @@ export const PaymentWrapper = styled.section`
   @media (min-width: ${props => props.theme.breakpoints.desktop}) {
     .content {
       padding: 60px 70px 90px;
-      flex-direction: row;
-      .info {
-        border: 2px solid ${props => props.theme.colors.blue_third};
-        border-radius: 15px;
-        padding: 20px;
-      }
-      .desc {
-        .func {
-          flex-direction: row;
+      .wrapper {
+        flex-direction: row;
+        .info {
+          border: 2px solid ${props => props.theme.colors.blue_third};
+          border-radius: 15px;
+          padding: 20px;
+        }
+        .desc {
+          .func {
+            flex-direction: row;
+          }
         }
       }
     }
@@ -119,9 +123,10 @@ export const PaymentWrapper = styled.section`
     .content {
       width: ${props => props.theme.maxWidth}px;
       margin: 0 auto;
-
-      .info {
-        flex-direction: row;
+      .wrapper {
+        .info {
+          flex-direction: row;
+        }
       }
     }
   }
